6176d4bf3c03be11ef86a3603cba981146f7c09c
Leo Moon <leomoondev@gmail.com>
2017-01-24 22:23:00 -0800

    Modified solution for assignment #1
    final commit

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -13,17 +13,23 @@ int main(int argc, const char * argv[]) {
     
     for(int i=1; i<101; i++) {
         
-        if(i % 3 == 0) {
+        if(i%3==0 && i%5==0) {
+            
+            printf("FizzBuzz\n");
+        }
+        
+        else if(i%3==0) {
+            
             printf("Fizz\n");
         }
-        else if (i % 5 == 0) {
+        
+        else if (i%5==0) {
+            
             printf("Buzz\n");
         }
-        else if( (i %3 == 0) && (i %5 == 0)) {
-            printf("FizzBuzz\n");
-            
-        }
+
         else {
+            
             printf("%d\n", i);
         }
     }
